82c9a3cdd5f58190fcb25a57ef885c4c65ebe30e
Thuomas <Gastonmansilla9@gmail.com>
2025-04-25 16:35:20 -0300

    gitignore modificacion

---
+++ Migrations/20250424101130_Entregas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SGM.Migrations
+{
+    /// <inheritdoc />
+    public partial class Entregas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entrega",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Remito = table.Column<string>(type: "TEXT", nullable: false),
+                    TrabajoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    CantidadRestante = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entrega", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entrega_Trabajo_TrabajoId",
+                        column: x => x.TrabajoId,
+                        principalTable: "Trabajo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entrega_TrabajoId",
+                table: "Entrega",
+                column: "TrabajoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entrega");
+        }
+    }
+}
